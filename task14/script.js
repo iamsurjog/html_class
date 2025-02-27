b81ae21b96c3b34b83b8c2463a7fb74d88fef371
Sujatro <88082977+iamsurjog@users.noreply.github.com>
2025-02-27 14:24:59 +0530

    completed task 14

--- task14/script.js
+++ task14/script.js
@@ -10,5 +10,29 @@ function getval() {
     var pattern = /(\W)/g;
     var sp = pattern.test(pass);
     var arr = [length, upper, lower, num, sp]; 
-    console.log(arr); 
+    var msgs = ["must be at least 8 characters", "must contain an uppercase character", "must contain a lowercase character", "must contain a number", "must contain a special character"];
+    var txt = "";
+    var count = 0;
+    for (i = 0; i<5; i++){
+        if(!arr[i]){
+            count +=1;
+            txt += "-";
+            txt += msgs[i];
+            txt += "\n";
+        }
+    }
+    if (count == 0){
+        var color = "green";
+    }else if(count >=3){
+        var color = "red";
+    }else{
+        var color = "yellow";
+    }
+     //   console.log(txt);
+    console.log(count);
+    console.log(color);
+    document.getElementById('indicator').style.color = color;
+    document.getElementById('indicator').innerText = txt;
 }
+
+
